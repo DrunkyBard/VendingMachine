f7b03422f4e3dec6f463224a9def90bb2f254af7
DrunkyBard <sattar.imamov@gmail.com>
2015-09-08 23:22:40 +0300

    - Add KO JS - Add data bindings for Vending Machine view - Partially implement Vending machine VM - Add ToJson serialization extension method

---
+++ src/VendingMachine/Utils/ObjectExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System.IO;
+using Newtonsoft.Json;
+
+namespace VendingMachineApp.Utils
+{
+    public static class ObjectExtensions
+    {
+        public static string ToJson(this object obj)
+        {
+            var js = JsonSerializer.Create(new JsonSerializerSettings());
+            var jw = new StringWriter();
+            js.Serialize(jw, obj);
+
+            return jw.ToString();
+        }
+
+    }
+}
